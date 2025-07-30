0c18759b11c18c28db59b766e3c9b4beb8660ab0
sundarR-developer <sundarravi0723@gmail.com>
2025-07-30 12:41:28 +0530

    Update QuizStart.js

--- src/components/QuizStart.js
+++ src/components/QuizStart.js
@@ -14,7 +14,7 @@ const QuizStart = () => {
 
   useEffect(() => {
     async function fetchExam() {
-      const examData = await getServerData(`https://quiz-server-8.onrender.com/api/exams/${examId}`);
+      const examData = await getServerData(`https://quiz-server-9.onrender.com/api/exams/${examId}`);
       setExam(examData);
     }
     fetchExam();
