3fb1fe5387701b420689e9f39d3c4d79cb93edde
sundarR-developer <sundarravi0723@gmail.com>
2025-08-03 15:08:39 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,38 +1,11 @@
-import { postServerData } from "../helper/helper";
-import * as Action from "../redux/result_reducer";
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-export const PushAnswer = (result) => async (dispatch) => {
-  try {
-    await dispatch(Action.pushResultAction(result));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const updateResult = (index) => async (dispatch) => {
-  try {
-    dispatch(Action.updateResultAction(index));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-/* insert user data */
-export const usePublishResult = (resultData) => {
-  const { result, username } = resultData;
-  // postServerData
-  (async () => {
-    try {
-      if (result.length === 0 || !username) {
-        throw new Error("Couldn't get result!");
-      }
-      await postServerData(
-        `https://quiz-server-9.onrender.com/api/result`,
-        resultData,
-        (data) => data
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  })();
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8081',
+      changeOrigin: true,
+    })
+  );
 };
